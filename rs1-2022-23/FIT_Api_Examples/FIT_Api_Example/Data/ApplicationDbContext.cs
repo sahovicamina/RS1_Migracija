6355d1094bb33db6ca66bace85c6f5f36c7df2f5
student <student@fit.ba>
2022-10-25 14:05:21 +0200

    Code

---
+++ rs1-2022-23/FIT_Api_Examples/FIT_Api_Example/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FIT_Api_Example.Modul2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FIT_Api_Example.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Drzava> Drzava { get; set; }
+        public DbSet<Opstina> Opstina { get; set; }
+        public DbSet<Student> Student { get; set; }
+
+        public ApplicationDbContext(
+            DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
